a182e189c06777fb24961a536fb06cc003647dee
AnthonyWaithaka <athumbi1@gmail.com>
2017-07-27 18:45:37 +0300

    TEST: Added some tests for views functions

--- test/test_views.py
+++ test/test_views.py
@@ -26,7 +26,7 @@ class ViewsTestCase(unittest.TestCase):
         """
         self.app = APP.test_client()
         self.app.testing = True
-    
+
     def tearDown(self):
         """Clean up the data from the tests
         """
@@ -44,28 +44,39 @@ class ViewsTestCase(unittest.TestCase):
         """Test HTTP GET request when user creates
         a new account
         """
-        response = self.app.post('/signup/', data={'email':'guy@yes.com',
-                                                   'user':'guy', 'pass':'aaa',
-                                                   'pass2':'aaa'})
+        response = self.app.post('/signup/', data={'email':'guy1@yes.com',
+                                                   'user':'guy1', 'pass':'aaa1',
+                                                   'pass2':'aaa1'})
         self.assertEqual(response.status_code, 200, msg="Did not return sign_up page")
 
     def test_signup_data(self):
         """Test return value when user creates
         a new account
         """
-        response = self.app.post('/signup/', data={'email':'guy@yes.com', 'user':'guy',
-                                                   'pass':'aaa', 'pass2':'aaa'})
+        response = self.app.post('/signup/', data={'email':'guy2@yes.com', 'user':'guy2',
+                                                   'pass':'aaa2', 'pass2':'aaa2'})
         self.assertTrue('login' in response.get_data(as_text=True), msg="Did not create account")
 
     def test_login_status_code(self):
         """Test HTTP GET request when user logs in to
         their account
         """
-        response = self.app.post('/signup/', data={'email':'guy@yes.com',
-                                                   'user':'guy', 'pass':'aaa',
-                                                   'pass2':'aaa'})
-        login_response = self.app.post('/login/', data={'logemail':'guy', 'logpassword':'aaa'})
-        self.assertEqual(login_response.status_code, 200, msg="Did not return bucket_lists page")
+        response = self.app.post('/signup/', data={'email':'guy3@yes.com',
+                                                   'user':'guy3', 'pass':'aaa3',
+                                                   'pass2':'aaa3'})
+        login_response = self.app.post('/login/', data={'logemail':'guy3', 'logpassword':'aaa3'},
+                                                        follow_redirects=True)
+        self.assertEqual(login_response.status_code, 302, msg="Did not return bucket_lists page")
+
+    def test_login_data(self):
+        """Test session data when user logs in to
+        their account
+        """
+        response = self.app.post('/signup/', data={'email':'guy4@yes.com',
+                                                   'user':'guy4', 'pass':'aaa4',
+                                                   'pass2':'aaa4'})
+        login_response = self.app.post('/login/', data={'logemail':'guy4', 'logpassword':'aaa4'})
+        self.assertEqual(session['loginuser'], 'guy4', msg="User did not log in successfully")
 
 if __name__ == '__main__':
     unittest.main()
